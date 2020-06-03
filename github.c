8d766327e56d3d046d247716f5c05d5d75b036c8
seong1542 <seong1542@naver.com>
2020-06-03 20:27:46 +0900

    산수하는 프로그램입니다

--- github.c
+++ github.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 int main()
 {
+	int a,b;
 	printf("hello world\n");
+	printf("µÎ ¼ö¸¦ ÀÔ·ÂÇÏ¼¼¿ä : ");
+	scanf("%d %d",&a,&b);
+	printf("µ¡¼À = %d + %d = %d\n",a,b,(a+b));
+	printf("»¬¼À = %d - %d = %d\n",a,b,(a-b));
+	printf("°ö¼À = %d * %d = %d\n",a,b,(a*b));
+	printf("³ª´°¼À = %d / %d = %d\n",a,b,(a/b));
 }
